d461cd81278da516de9bb5f1e6336b18c680bfb8
MbaliFelecia <ramakgashambali@gmail.com>
2023-11-20 11:38:48 -0800

    update to user

--- models/user.py
+++ models/user.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-import hashlib  # noqa
-import models  # noqa
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from os import getenv
-from sqlalchemy import Column, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
 
 
 class User(BaseModel, Base):
-    """MySQL database user. Inherits from SQLAlchemy Base and links to the
+    """MySQL database user. Inherits from SQLAlchemy Base and links
     MySQL table users.
       Attributes:
       __tablename__ (str): The name of the MySQL table to store users.
@@ -20,38 +20,12 @@ class User(BaseModel, Base):
       places (sqlalchemy relationship): The User-Place relationship.
       reviews (sqlalchemy relationship): The User-Review relationship.
     """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'users'
-        email = Column(String(128),
-                       nullable=False)
-        password = Column('password',
-                          String(128),
-                          nullable=False)
-        first_name = Column(String(128),
-                            nullable=True)
-        last_name = Column(String(128),
-                           nullable=True)
-        places = relationship("Place",
-                              back_populates="user",
-                              cascade="all, delete")
-        reviews = relationship("Review",
-                               back_populates="user",
-                               cascade="all, delete")
-    else:
-        email = ""
-        _password = ""
-        first_name = ""
-        last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
-
-    # @property
-    # def password(self):
-    #     return self._password
-
-    # @password.setter
-    # def password(self, pwd):
-    #     """hashing password values"""
-    #     self._password = pwd
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+        places = relationship("Place", cascade='all, delete, delete-orphan',
+                              backref="user")
+        reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                               backref="user")
