1b9634e4db067b2a658bdf445462ec35d6881fd6
MbaliFelecia <ramakgashambali@gmail.com>
2023-11-20 16:43:33 -0800

    update to amenities and add the relationship

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import models  # noqa
 from models.base_model import BaseModel, Base
-from os import getenv
-from sqlalchemy import Table, Column, String, ForeignKey  # noqa
-from sqlalchemy.orm import relationship  # noqa
+from models.place import place_amenity
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
@@ -14,18 +13,6 @@ class Amenity(BaseModel, Base):
         name (sqlalchemy String): The amenity name.
         place_amenities (sqlalchemy relationship): Place-Amenity relationship.
     """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        from models.place import place_amenity
-        __tablename__ = 'amenities'
-        name = Column(String(128),
-                      nullable=False)
-        place_amenities = relationship(
-            'Place',
-            secondary=place_amenity, back_populates='amenities'
-        )
-    else:
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes Amenity"""
-        super().__init__(*args, **kwargs)
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
