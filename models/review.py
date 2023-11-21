44b633c40fc856ab4b8116fda4c11e321bf885ed
MbaliFelecia <ramakgashambali@gmail.com>
2023-11-20 16:27:25 -0800

    update to review using sql

--- models/review.py
+++ models/review.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from os import getenv
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, ForeignKey, String
-from sqlalchemy.orm import relationship  # noqa
+from sqlalchemy import Column, ForeignKey, String, Integer, Float
+from sqlalchemy.orm import relationship 
 
 
 class Review(BaseModel, Base):
@@ -14,23 +14,9 @@ class Review(BaseModel, Base):
         place_id (sqlalchemy String): The review's place id.
         user_id (sqlalchemy String): The review's user id.
     """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'reviews'
-        text = Column(String(1024),
-                      nullable=False)
-        place_id = Column(String(60),
-                          ForeignKey('places.id'),
-                          nullable=False)
-        user_id = Column(String(60),
-                         ForeignKey('users.id'),
-                         nullable=False)
+    __tablename__ = "reviews"
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         user = relationship('User', back_populates='reviews')
         place = relationship('Place', back_populates='reviews')
-    else:
-        text = ""
-        place_id = ""
-        user_id = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes Review"""
-        super().__init__(*args, **kwargs)
